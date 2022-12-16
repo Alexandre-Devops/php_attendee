bebeb7a439ee267c601158eba0d2d8ef7b0d805f
Alexandre Francisco <alexandredelucasfrancisco@gmail.com>
2022-12-16 16:07:02 +0000

    Adicionando o formulario de edição

--- edit.php
+++ edit.php
@@ -19,7 +19,8 @@ else
 
 
     <h1 class="text-center">Edit Record</h1>
-    <form method="POST" action="success.php" >
+    <form method="post" action="editpost.php" >
+    <input type="hidden" name="id" value="<?php echo $attendee['attendee_id'] ?>" >
     <div class="mb-3">
     <label for="fristname" class="form-label">Frist Name</label>
     <input type="text" name="fristname" class="form-control" value="<?php echo $attendee['fristname'] ?>" id="fristname">
@@ -39,25 +40,20 @@ else
     <label for="specialty" class="form-label">Area of Expertise</label>
     <select class="form-control"  id="specialty" name="specialty">
     <?php while ($r = $results->fetch(PDO::FETCH_ASSOC)) {  ?>
-
-   
-    
-    <option value="<?php echo $r['specialty_id']?>"> <?php echo $r['name']?> </option>
-
-
-<?php } ?>
-
+    <option value="<?php echo $r['specialty_id']?>" <?php if($r['specialty_id'] == $attendee['specialty_id']) echo 'selected' ?>>
+    <?php echo $r['name']?> </option>
+    <?php } ?>
     </select>
        
   </div>
   <div class="mb-3">
     <label for="email" class="form-label">Email address</label>
-    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+    <input type="email" name="email" class="form-control" value="<?php echo $attendee['emailadress'] ?>" id="email" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="phone" class="form-label">Contact Number</label>
-    <input type="text" name="phone" class="form-control" id="phone" aria-describedby="phoneHelp">
+    <input type="text" name="phone" class="form-control" value="<?php echo $attendee['contactnumber'] ?>" id="phone" aria-describedby="phoneHelp">
     <div id="phoneHelp" class="form-text">We'll never share your phone with anyone else.</div>
   </div>
   
